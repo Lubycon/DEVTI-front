c364a7e597ed66aec4421818360e9b6598c89ce1
geonwoo.jeong <doscmDev@gmail.com>
2021-04-18 16:37:30 +0900

    refactor: divide config of react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,18 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { Hydrate } from 'react-query/hydration';
 
-const App = ({ Component, pageProps }:AppProps) => {
-  const queryClient = new QueryClient();
-  queryClient.setDefaultOptions({
-    queries: {
-      staleTime: Infinity,
-      refetchOnWindowFocus: false,
-      refetchInterval: false,
-    },
-  });
+import { queryClient } from '../config/reactQuery';
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-      </Hydrate>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
-};
+const App = ({ Component, pageProps }:AppProps) => (
+
+  <QueryClientProvider client={queryClient}>
+    <Hydrate state={pageProps.dehydratedState}>
+      <Component {...pageProps} />
+    </Hydrate>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+);
 
 export default App;
