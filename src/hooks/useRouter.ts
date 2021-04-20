29175d0376ec5c83611c19d98ae7bb28d406e55d
geonwoo.jeong <doscmDev@gmail.com>
2021-04-21 00:19:41 +0900

    refactor: wrap useRouter to add generic

---
+++ src/hooks/useRouter.ts
@@ -0,0 +1,18 @@
+import { ParsedUrlQuery } from 'querystring';
+
+import { useRouter as nextRouter } from 'next/router';
+
+type CastQuery<T> = T & ParsedUrlQuery;
+
+const useRouter = <T>() => {
+  const { query: nextQuery, push: nextPush, ...route } = nextRouter();
+
+  const query = nextQuery as CastQuery<T>;
+
+  return {
+    ...route,
+    query,
+  };
+};
+
+export default useRouter;
