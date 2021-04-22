b701a8fde8bd8b480c3192565410f021f8a23473
geonwoo.jeong <doscmDev@gmail.com>
2021-04-23 03:35:52 +0900

    refactor: rename Component
    Co-Authored-By: Yurim Jalynne Jin <jayjinjay@gmail.com>

--- src/components/molecules/ThumbnailCard.tsx
+++ src/components/molecules/ThumbnailCard.tsx
@@ -1,12 +1,12 @@
 import { Flex, Image, Text } from 'rebass';
 
-interface CircleCardProps {
+interface ThumbnailCardProps {
   imageUrl: string;
   description: string;
   alt: string;
 }
 
-const CircleCard = ({ imageUrl, description, alt }: CircleCardProps) => (
+const ThumbnailCard = ({ imageUrl, description, alt }: ThumbnailCardProps) => (
   <Flex flexDirection="column" mx={45}>
     <Image src={imageUrl} alt={alt} width={247} />
     <Text mt={40} fontSize={20}>
@@ -15,4 +15,4 @@ const CircleCard = ({ imageUrl, description, alt }: CircleCardProps) => (
   </Flex>
 );
 
-export default CircleCard;
+export default ThumbnailCard;
