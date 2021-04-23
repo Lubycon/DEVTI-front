199cc90562e4f615eed03dde39258f9a71add088
geonwoo.jeong <doscmDev@gmail.com>
2021-04-23 19:59:59 +0900

    style: apply color palette in theme
    Co-Authored-By: Yurim Jalynne Jin <jayjinjay@gmail.com>

--- src/styles/forms.ts
+++ src/styles/forms.ts
@@ -4,13 +4,15 @@ const forms = {
   },
   textarea: {
     variant: 'forms.default',
-    border: '1px solid #e0e0e0',
+    border: '1px solid',
+    borderColor: 'gray.1',
     outline: 'none',
     resize: 'none',
   },
   input: {
     variant: 'forms.default',
-    border: '1px solid #e0e0e0',
+    border: '1px solid',
+    borderColor: 'gray.1',
   },
   label: {
     display: 'flex',
