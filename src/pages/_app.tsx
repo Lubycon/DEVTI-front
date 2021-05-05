172d1e454443e1f0573c8f154934d585622657b8
geonwoo.jeong <doscmdev@gmail.com>
2021-05-06 04:37:23 +0900

    feat: set isMobile to global value

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,39 @@
 import { ThemeProvider } from 'emotion-theming';
+import { NextComponentType, NextPageContext } from 'next';
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { Hydrate } from 'react-query/hydration';
 
 import { queryClient } from '../config/reactQuery';
+import isMobileDetect from '../libs/server/isMobileDetect';
 import GlobalStyle from '../styles/GlobalStyle';
 import theme from '../styles/theme';
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <QueryClientProvider client={queryClient}>
-    <Hydrate state={pageProps.dehydratedState}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </Hydrate>
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
-);
+interface Context {
+  Component: NextComponentType<NextPageContext>;
+  ctx: NextPageContext;
+}
+
+const App = ({ Component, pageProps }: AppProps) => {
+  queryClient.setQueryData('isMobile', pageProps.isMobile);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+App.getInitialProps = async ({ ctx: { req } }: Context) => {
+  const isMobile = isMobileDetect(req);
+  return { pageProps: { isMobile } };
+};
 
 export default App;
