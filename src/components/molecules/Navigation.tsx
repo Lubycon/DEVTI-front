5c2d31dab9a7bda5aff1cb0c1d214b3c82ddc592
geonwoo.jeong <doscmdev@gmail.com>
2021-05-06 04:42:46 +0900

    feat: add useScrollTo

--- src/components/molecules/Navigation.tsx
+++ src/components/molecules/Navigation.tsx
@@ -1,16 +1,18 @@
 import { Button, Flex, Text } from 'rebass';
 
-import useDeviceDetect from '../../hooks/useDeviceDetect';
+import useScrollTo from '../../hooks/useScrollTo';
 
 const Navigation = () => {
-  const { isMobile } = useDeviceDetect();
+  const { handleExecuteScroll } = useScrollTo('test');
 
   return (
     <Flex as="nav" variant="navigation">
       <Text fontWeight={800} fontSize="30px" flex={2} color="blue.0">
         DEVTI
       </Text>
-      {!isMobile && <Button variant="blue">검사하기</Button>}
+      <Button variant="blue" fontWeight={700} onClick={handleExecuteScroll}>
+        검사하기
+      </Button>
     </Flex>
   );
 };
