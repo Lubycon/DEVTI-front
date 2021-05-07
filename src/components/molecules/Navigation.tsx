208511b7945958d6c97fd3ad1ef4084052f42b40
geonwoo.jeong <doscmdev@gmail.com>
2021-05-08 03:01:02 +0900

    style: float navigation

--- src/components/molecules/Navigation.tsx
+++ src/components/molecules/Navigation.tsx
@@ -1,12 +1,42 @@
+import { useEffect, useRef } from 'react';
 import { Button, Flex, Text } from 'rebass';
 
 import useScrollTo from '../../hooks/useScrollTo';
 
 const Navigation = () => {
+  const ref = useRef<HTMLElement>();
+
   const { handleExecuteScroll } = useScrollTo('test');
 
+  const handleScroll = () => {
+    let lastScrollTop = 0;
+    const checkScrolLDirection = () => {
+      const st = window.pageYOffset || document.documentElement.scrollTop;
+      const nav = ref.current;
+      if (nav) {
+        if (st > lastScrollTop) {
+          nav.style.backgroundColor = 'white';
+          nav.style.padding = '0 40px';
+        } else {
+          nav.style.padding = '0 140px';
+          nav.style.backgroundColor = 'inherit';
+        }
+      }
+      lastScrollTop = st <= 0 ? 0 : st;
+    };
+    checkScrolLDirection();
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <Flex as="nav" variant="navigation">
+    <Flex ref={ref} as="nav" variant="navigation">
       <Text fontWeight={800} fontSize="30px" flex={2} color="blue.0">
         DEVTI
       </Text>
