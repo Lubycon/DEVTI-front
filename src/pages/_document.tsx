1b701a67b0f6b6ac88aa1f6d93d07f4276bc0795
geonwoo.jeong <doscmdev@gmail.com>
2021-05-10 07:33:13 +0900

    feat: add meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,12 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css" rel="stylesheet" type="text/css" />
+          <meta property="og:title" content="DEVTI 데브티아이" />
+          <meta property="og:description" content="나에게 딱 맞는 개발자 직군을 테스트를 통해 찾아보세요!" />
+          <meta
+            property="og:image"
+            content="https://user-images.githubusercontent.com/39829378/117588913-879ba280-b161-11eb-81f1-d120b79d2f78.png"
+          />
         </Head>
         <body>
           <Main />
