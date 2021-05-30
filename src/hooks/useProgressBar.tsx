a5c1d15810a07952d5d58228ca58e609ebefe30c
geonwoo.jeong <doscmdev@gmail.com>
2021-05-31 02:27:43 +0900

    refactor: add resetGage

--- src/hooks/useProgressBar.tsx
+++ src/hooks/useProgressBar.tsx
@@ -5,11 +5,13 @@ import ProgressBar, { ProgressBarProps } from '~atoms/ProgressBar';
 const useProgressBar = ({ totalCount, currentCount, minCount, ...props }: ProgressBarProps) => {
   const [current, setCurrent] = useState(currentCount ?? 0);
 
+  const resetGage = () => setCurrent(0);
   const handleIncreaseGage = () => setCurrent(current + 1);
+
   const handleDecreaseGage = () => setCurrent(current - 1);
 
   const renderProgressBar = () => <ProgressBar totalCount={totalCount} currentCount={current} minCount={minCount} {...props} />;
-  return { renderProgressBar, handleIncreaseGage, handleDecreaseGage };
+  return { renderProgressBar, handleIncreaseGage, handleDecreaseGage, resetGage };
 };
 
 export default useProgressBar;
