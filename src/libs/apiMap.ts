1b814fe48626275a2670381c635d86d0edb0b67f
geonwoo.jeong <doscmdev@gmail.com>
2021-06-19 23:38:33 +0900

    feat: add question api

--- src/libs/apiMap.ts
+++ src/libs/apiMap.ts
@@ -1,22 +1,30 @@
 export type UrlMapKey = keyof typeof urlMap;
 
-const URL = 'https://api.devti.kr';
+export const BASE_URL = 'http://13.209.35.71:8090';
 
 const urlMap = {
   betaSignUp: {
-    url: `${URL}/survey`,
+    url: `${BASE_URL}/survey`,
     method: 'POST',
   },
   getBucketTest: {
-    url: `${URL}/bucket-test-type/:entryPoint`,
+    url: `${BASE_URL}/bucket-test-type/:entryPoint`,
     method: 'GET',
   },
   getSharedCount: {
-    url: `${URL}/event-log/:eventType`,
+    url: `${BASE_URL}/event-log/:eventType`,
     method: 'GET',
   },
   postEventLog: {
-    url: `${URL}/event-log`,
+    url: `${BASE_URL}/event-log`,
+    method: 'POST',
+  },
+  getAllQuestion: {
+    url: `${BASE_URL}/question/all`,
+    method: 'GET',
+  },
+  postQuestionResult: {
+    url: `${BASE_URL}/devti`,
     method: 'POST',
   },
 };
